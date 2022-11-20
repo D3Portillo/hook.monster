386a50fbfc2b6e41f76ea257a0b96d3fd27d355c
D3Portillo <d3portillo@gmail.com>
2022-11-20 11:20:35 +0100

    feat(Button.tsx): component now expects flavor to set custom styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,23 @@
 import { PropsWithChildrenCx } from "@/types/shared"
 import { classnames } from "@/lib/helper"
 
-function Button({ children, className }: PropsWithChildrenCx) {
+function Button({
+  children,
+  className,
+  flavor = "pink",
+}: PropsWithChildrenCx<{
+  flavor?: "pink" | "black" | "whiter"
+}>) {
   return (
     <button
       className={classnames(
         className,
-        "block rounded-full bg-pink-500 px-8 py-3 font-medium text-white transition hover:bg-pink-600"
+        flavor === "pink" && "bg-pink-500 hover:bg-pink-600",
+        flavor === "black" && "bg-black hover:bg-opacity-80",
+        flavor === "whiter"
+          ? "bg-zinc-50 border border-zinc-50 text-gray-800 hover:text-black hover:bg-white hover:border-zinc-100"
+          : "text-white",
+        "rounded-full px-8 py-3 font-medium transition"
       )}
     >
       {children}
